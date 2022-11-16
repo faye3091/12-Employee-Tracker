b69409a9222f3fe60b4bb2e6c850d13378e46f17
Vanessa <faye_3091@yahoo.com>
2022-11-15 22:47:10 -0500

    i accidentally deleted config.js on the last push

---
+++ utils/config.js
@@ -0,0 +1,15 @@
+//Dependencies
+const mysql = require("mysql");
+
+//Connection to server
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  //password is hidden in github
+  password: "password",
+  database: "employees",
+});
+
+//Export
+module.exports = connection;
