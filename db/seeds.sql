c95d93673e833cc40dd77e00b3458029973134e3
Vanessa <faye_3091@yahoo.com>
2022-11-15 22:38:05 -0500

    few changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,8 @@
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-);
+INSERT INTO department(departmen_name)
+VALUES("Engineering"),("Sales"), ("Finance"), ("Legal");
 
-CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departmen(id)
-);
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 90000, 1), ("Senior Engineer", 120000, 1);
 
-CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
-    CONSTAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(manager_id),
-);
+INSERt INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 2), ('Charlotte', 'Crow', 1, null), ('Mike', 'Seaux', 1, 2), ('Jimmy', 'Jones', 2, 2);
